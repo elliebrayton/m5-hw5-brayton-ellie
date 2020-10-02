4bfbba4fadcb8a8346f1f32aa9e751f6164122b7
Ellie Brayton <ebrayton@ufl.edu>
2020-10-02 15:58:29 -0500

    Worked on part 1 of hw

---
+++ main.js
@@ -0,0 +1,12 @@
+var pizza = {
+    crust: "garlic",
+    cheese: "mozzarella",
+    sauce: "tomato"
+};
+
+pizza.addToppings = function(){
+    console.log("Please add pepperoni and sausage");
+}
+
+console.log(pizza.addToppings());
+
